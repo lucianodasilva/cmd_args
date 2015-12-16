b619364ee9e53b815127c76e87242136fd9896af
Luciano da Silva <luciano.silva@live.com.pt>
2015-12-16 19:02:53 +0000

    aparently working version of backtracking

--- example/main.cpp
+++ example/main.cpp
@@ -17,329 +17,41 @@
 
 using namespace std;
 
-//
-//namespace command_line {
-//
-//	namespace details {
-//
-//		// struture 
-//		using range_it = std::reverse_iterator < char ** >;
-//
-//		struct range_t {	
-//			range_it it, end;
-//
-//			inline bool ended () const { return it == end; }
-//
-//			inline const char * consume() {
-//				return *it++;
-//			}
-//
-//			inline range_t union_with(const range_t & r) const {
-//				if (distance(it, r.it) > 0)
-//					return range_t{ it, r.it + 1 };
-//				else
-//					return range_t{ r.it, it + 1 };
-//			}
-//
-//			inline char ** forward_begin() const {
-//				return it.base() + 1;
-//			}
-//			
-//			inline char ** forward_end() const {
-//				return end.base() + 1;
-//			}
-//
-//		};
-//
-//		struct base_setter_t {
-//			using unique = unique_ptr < base_setter_t >;
-//			virtual void set(const range_t & r) = 0;
-//		};
-//
-//		template < class _dest_t, class _t > 
-//		struct setter_t : public base_setter_t {
-//			_t _dest_t::*address;
-//
-//			setter_t(_t _dest_t::*address_v) : address(address) {}
-//
-//			virtual void set(const range_t & r) {}
-//		};
-//
-//		template < class _dest_t, class _t, class ... _other_tv >
-//		struct setter_t < _dest_t, vector < _t, _other_tv...> > : public base_setter_t {
-//			vector < _t, _other_tv...> _dest_t::*address;
-//			setter_t(vector < _t, _other_tv...> _dest_t::*address_v) : address(address) {}
-//
-//			virtual void set(const range_t & r) {}
-//		};
-//
-//		struct exec_node {
-//			range_t range;
-//			base_setter_t::unique setter;
-//		};
-//
-//		using exec_queue_t = vector < exec_node >;
-//
-//		struct context_state {
-//			uint32_t queue_len;
-//			range_t range;
-//		};
-//
-//		struct context {
-//			exec_queue_t	exec_queue;
-//			range_t			range;
-//
-//			inline context_state state() const {
-//				return{
-//					exec_queue.size(),
-//					range
-//				};
-//			}
-//
-//			inline void restore(const context_state & state) {
-//				exec_queue.resize(state.queue_len);
-//				range = state.range;
-//			}
-//		};
-//
-//		// setter_t
-//		template < class _exp_t, class _dest_t, class _t >
-//		struct setter_op {
-//			_exp_t exp;
-//			_t _dest_t::* address;
-//
-//			inline bool eval(context & cxt) const {
-//				auto state = cxt.state();
-//				if (exp.eval(cxt)) {
-//					auto range = state.range.union_with(cxt.range);
-//					cxt.exec_queue.push_back( exec_node {
-//						range,
-//						make_unique < setter_t < _dest_t, _t > > (address)
-//					});
-//					return true;
-//				}
-//				return false;
-//			}
-//
-//			inline setter_op(const _exp_t & e, _t _dest_t::*address_v) : exp(e), address(address_v) {}
-//		};
-//
-//		// sequence
-//		template < class _lhe_t, class _rhe_t >
-//		struct seq_op  {
-//			_lhe_t lhe;
-//			_rhe_t rhe;
-//
-//			inline bool eval (context & cxt) const {
-//				return rhe.eval(cxt) && lhe.eval(cxt);
-//			}
-//		};
-//
-//		template < class _lhe_t, class _rhe_t >
-//		auto operator > (const _lhe_t & l, const _rhe_t & r) {
-//			return seq_op < _lhe_t, _rhe_t > { l, r };
-//		}
-//
-//		// alternation
-//		template < class _lhe_t, class _rhe_t >
-//		struct alt_op {
-//			_lhe_t lhe;
-//			_rhe_t rhe;
-//
-//			inline bool eval(context & cxt) const {
-//				auto state = cxt.state();
-//				bool valid = rhe.eval(cxt);
-//
-//				if (!valid) {
-//					cxt.restore(state);
-//					valid = lhe.eval(cxt);
-//				}
-//
-//				return valid;
-//			}
-//		};
-//
-//		template < class _lhe_t, class _rhe_t >
-//		auto operator | (const _lhe_t & l, const _rhe_t & r) {
-//			return alt_op < _lhe_t, _rhe_t > { l, r };
-//		}
-//
-//		// zero or one
-//		template < class _exp_t >
-//		struct z_one_op {
-//			_exp_t exp;
-//
-//			inline bool eval(context & cxt) const {
-//				auto state = context_state();
-//
-//				if (!exp.eval(cxt))
-//					cxt.restore(state);
-//
-//				return true;
-//			}
-//		};
-//
-//		template < class _exp_t >
-//		auto operator - (const _exp_t & e) {
-//			return z_one_op < _exp_t > { e };
-//		}
-//
-//		// one or n
-//		template < class _exp_t >
-//		struct one_n_op {
-//			_exp_t exp;
-//
-//			inline bool eval(context & cxt) const {
-//				bool one_valid = exp.eval(cxt);
-//
-//				if (one_valid) {
-//					auto state = context_state();
-//
-//					do {
-//						state = cxt.state ();
-//					} while (exp.eval(cxt));
-//
-//					cxt.restore(state);
-//				}
-//
-//				return one_valid;
-//			}
-//		};
-//
-//		template < class _exp_t >
-//		auto operator + (const _exp_t & e) {
-//			return one_n_op < _exp_t > { e };
-//		}
-//
-//		// zero or n
-//		template < class _exp_t >
-//		struct z_n_op {
-//			_exp_t exp;
-//
-//			inline bool eval(context & cxt) const{
-//				auto state = context_state();
-//
-//				do {
-//					state = cxt.state();
-//				} while (exp.eval(cxt));
-//
-//				cxt.restore(state);
-//				return true;
-//			}
-//		};
-//
-//		template < class _exp_t >
-//		auto operator * (const _exp_t & e) {
-//			return z_n_op < _exp_t > { e };
-//		}
-//
-//		// scaners
-//		struct value_t {
-//			const char * key;
-//
-//			inline bool eval(context & cxt) const { 
-//				if (cxt.range.ended())
-//					return false;
-//
-//				auto v = cxt.range.consume();
-//				return strcmp (v, key) == 0;
-//			}
-//
-//			template < class _dest_t, class _t >
-//			inline auto operator [] (_t _dest_t::*address) {
-//				return setter_op < value_t, _dest_t, _t >(*this, address);
-//			}
-//		};
-//		
-//		struct any_t {
-//
-//			inline bool eval(context & cxt) const { 
-//				if (cxt.range.ended())
-//					return false;
-//
-//				cxt.range.consume();
-//				return true;
-//			}
-//
-//			template < class _dest_t, class _t >
-//			inline auto operator [] (_t _dest_t::*address) {
-//				return setter_op < any_t, _dest_t, _t >(*this, address);
-//			}
-//		};
-//
-//		// Using
-//		/* template < class _exp_t >
-//		struct using_t {
-//			_exp_t exp;
-//
-//			inline bool eval(context & cxt) const {
-//				return exp.eval(cxt);
-//			}
-//
-//			template < class _dest_t, class _t >
-//			inline auto operator [] (_t _dest_t::*address) {
-//				
-//			}
-//
-//		}; */
-//	}
-//
-//	inline details::value_t key(const char * k) {
-//		return { k };
-//	}
-//
-//	inline details::any_t any() { return{}; }
-//
-//	template < class _t >
-//	inline bool parse(const _t & exp, int arg_c, char ** arg_v ) {
-//		using namespace details;
-//
-//		context cxt;
-//
-//		// reverse range
-//		cxt.range = {
-//			reverse_iterator < char ** >(arg_v + arg_c),
-//			reverse_iterator < char ** >(+arg_v) // ignore first item
-//		};
-//		
-//		bool has_valid = exp.eval(cxt) && cxt.range.ended ();
-//
-//		return false;
-//	}
-//
-//}
-
-
 namespace command_line {
 
 	namespace details {
 
-		// struture 
+		// domain iterator range 
 		struct range_t {
 			char 
 				** it, 
 				** end;
 
-			inline bool ended() const { return it == end; }
+			// returns true if range is length is zero
+			inline bool finished () const { return it == end; }
 
+			// consume first item in range and return it's value
 			inline const char * consume() {
 				return *it++;
 			}
-
-			inline range_t union_with(const range_t & r) const {
-				if (distance(it, r.it) > 0)
-					return range_t{ it, r.it + 1 };
-				else
-					return range_t{ r.it, it + 1 };
-			}
-
 		};
 
+		// merge two ranges together
+		// (TODO: this name and operator does not correctly represent method)
+		inline range_t operator && (const range_t & v1, const range_t & v2) {
+			if (distance(v1.it, v2.it) > 0)
+				return{ v1.it, v2.it };
+			else
+				return{ v2.it, v1.it };
+		}
+
+		// value setter abstraction
 		struct base_setter_t {
 			using unique = unique_ptr < base_setter_t >;
 			virtual void set(const range_t & r) = 0;
 		};
 
+		// setter specialization for single items
 		template < class _dest_t, class _t >
 		struct setter_t : public base_setter_t {
 			_t _dest_t::*address;
@@ -349,6 +61,7 @@ namespace command_line {
 			virtual void set(const range_t & r) {}
 		};
 
+		// setter specialization for vectors
 		template < class _dest_t, class _t, class ... _other_tv >
 		struct setter_t < _dest_t, vector < _t, _other_tv...> > : public base_setter_t {
 			vector < _t, _other_tv...> _dest_t::*address;
@@ -357,42 +70,85 @@ namespace command_line {
 			virtual void set(const range_t & r) {}
 		};
 
+		// solution building/execution node
 		struct solution_node {
 			range_t range;
 			base_setter_t::unique setter;
 
-			uint32_t seq_index;
+			int sequence_index;
 		};
 
-		using solution_tree_t = vector < solution_node >;
+		struct solution_tree_state_t {
+			size_t	tree_size;
+			int		parent_node;
+		};
+
+		struct solution_tree_t {
+		private:
+			vector < solution_node > tree;
+			int32_t parent_node;
+		public:
+
+			inline solution_node & operator [] (size_t i) {
+				return tree[i];
+			}
+
+			inline size_t size() const {
+				return tree.size();
+			}
+
+			inline solution_tree_t() : parent_node(-1) {}
+
+			inline void add_node (const range_t & rng, base_setter_t * setter ) {
+				tree.push_back(solution_node{
+					rng,
+					unique_ptr < base_setter_t >(setter),
+					parent_node++
+				});
+			}
+
+			inline solution_tree_state_t state () const {
+				return{
+					tree.size(),
+					parent_node
+				};
+			}
+
+			inline void restore(const solution_tree_state_t & state) {
+				tree.resize(state.tree_size);
+				parent_node = state.parent_node;
+			}
+
+			inline void backtrack(const solution_tree_state_t & state) {
+				parent_node = state.parent_node;
+			}
+
+		};
 
 		struct context_state {
-			uint32_t solution_len;
-			uint32_t parent_node;
-			range_t	 range;
+			solution_tree_state_t	tree_state;
+			range_t					range;
 		};
 
 		struct context {
-			solution_tree_t	solution_tree;
-			uint32_t		parent_node;
 			range_t			range;
+			solution_tree_t	solution_tree;
 
 			inline context_state state() const {
-				return{
-					static_cast < uint32_t > (solution_tree.size()),
-					parent_node,
+				return {
+					solution_tree.state(),
 					range
 				};
 			}
 
 			inline void restore(const context_state & state) {
-				solution_tree.resize(state.solution_len);
+				solution_tree.restore(state.tree_state);
 				range = state.range;
 			}
 
 			inline void backtrack(const context_state & state) {
+				solution_tree.backtrack(state.tree_state);
 				range = state.range;
-				parent_node = state.parent_node;
 			}
 		};
 
@@ -405,18 +161,25 @@ namespace command_line {
 			inline bool eval(context & cxt) const {
 				auto state = cxt.state();
 				if (exp.eval(cxt)) {
-					auto range = state.range.union_with(cxt.range);
-					cxt.solution_tree.push_back(solution_node {
-						range,
-						make_unique < setter_t < _dest_t, _t > >(address),
-						cxt.parent_node++
-					});
+					cxt.solution_tree.add_node(
+						state.range && cxt.range, 
+						new setter_t < _dest_t, _t >(address)
+					);
+
 					return true;
 				}
 				return false;
 			}
 
 			inline setter_op(const _exp_t & e, _t _dest_t::*address_v) : exp(e), address(address_v) {}
+		};
+
+		template < class _derived_t >
+		struct setter_container_t {
+			template < class _dest_t, class _t >
+			inline auto operator [] (_t _dest_t::*address) {
+				return setter_op < _derived_t, _dest_t, _t >(*static_cast < _derived_t * > (this), address);
+			}
 		};
 
 		// sequence
@@ -442,14 +205,28 @@ namespace command_line {
 			_rhe_t rhe;
 
 			inline bool eval(context & cxt) const {
-				auto state = cxt.state();
-				bool valid = lhe.eval(cxt);
+				auto
+					state = cxt.state(),
+					lhe_state = state;
 
-				cxt.backtrack(state);
+				bool 
+					valid = lhe.eval(cxt),
+					backtrack_lhe_state = valid;
+
+				if (valid) {
+					lhe_state = cxt.state();
+					cxt.backtrack(state);
+				} else {
+					cxt.restore(state);
+				}
+
 				valid |= rhe.eval(cxt);
 
 				if (!valid)
 					cxt.restore(state);
+
+				if (backtrack_lhe_state)
+					cxt.backtrack(lhe_state);
 
 				return valid;
 			}
@@ -519,7 +296,9 @@ namespace command_line {
 					state = cxt.state();
 				} while (exp.eval(cxt));
 
+				// restore last correct state
 				cxt.restore(state);
+
 				return true;
 			}
 		};
@@ -530,54 +309,75 @@ namespace command_line {
 		}
 
 		// scaners
-		struct value_t {
+		struct value_t : public setter_container_t < value_t > {
 			const char * key;
 
+			inline value_t ( const char * key_v ) : key (key_v) {}
+
 			inline bool eval(context & cxt) const {
-				if (cxt.range.ended())
+				if (cxt.range.finished ())
 					return false;
 
 				auto v = cxt.range.consume();
 				return strcmp(v, key) == 0;
 			}
-
-			template < class _dest_t, class _t >
-			inline auto operator [] (_t _dest_t::*address) {
-				return setter_op < value_t, _dest_t, _t >(*this, address);
-			}
 		};
 
-		struct any_t {
+		struct any_t : public setter_container_t < any_t > {
 
 			inline bool eval(context & cxt) const {
-				if (cxt.range.ended())
+				if (cxt.range.finished())
 					return false;
 
 				cxt.range.consume();
 				return true;
 			}
 
-			template < class _dest_t, class _t >
-			inline auto operator [] (_t _dest_t::*address) {
-				return setter_op < any_t, _dest_t, _t >(*this, address);
-			}
 		};
 
-		// Using
-		/* template < class _exp_t >
-		struct using_t {
-		_exp_t exp;
+		// solution node inversion and execution
+		inline bool execute(details::context & cxt, details::range_t & arg_range) {
 
-		inline bool eval(context & cxt) const {
-		return exp.eval(cxt);
+			auto & tree = cxt.solution_tree;
+			int solution_index = -1;
+
+			for (
+				int i = 0;
+				i < static_cast < int > (tree.size());
+				++i
+			) {
+				if (tree[i].range.end == arg_range.end) {
+					solution_index = i;
+					break;
+				}
+			}
+
+			if (solution_index == -1)
+				return false;
+
+			// reorder instructions and execute
+			{
+				int n = solution_index;
+				int i = tree[n].sequence_index;
+				tree[n].sequence_index = -1;
+
+				do {
+					int t = tree[i].sequence_index;
+					tree[i].sequence_index = n;
+					n = i;
+					i = t;
+				} while (i != -1);
+
+				while (i != -1) {
+					auto & node = tree[i];
+					node.setter->set(node.range);
+					i = node.sequence_index;
+				}
+			}
+
+			return true;
 		}
 
-		template < class _dest_t, class _t >
-		inline auto operator [] (_t _dest_t::*address) {
-
-		}
-
-		}; */
 	}
 
 	inline details::value_t key(const char * k) {
@@ -586,25 +386,22 @@ namespace command_line {
 
 	inline details::any_t any() { return{}; }
 
-	template < class _t >
-	inline bool parse(const _t & exp, int arg_c, char ** arg_v) {
+	template < class _exp_t >
+	inline bool parse(const _exp_t & exp, int arg_c, char ** arg_v) {
 		using namespace details;
 
-		context cxt;
-
-		cxt.parent_node = 0;
-		// reverse range
-		cxt.range = {
-			arg_v,
-			+arg_v + arg_c// ignore first item
+		auto s_range = range_t {
+			+arg_v + 1,	// ignore first item
+			+arg_v + arg_c
 		};
 
-		// check for valid and complete solutions
-		bool has_valid_solutions = exp.eval(cxt) && cxt.range.ended();
+		context cxt = { s_range };
 
-		// assemble complete solution
+		// evaluate solutions
+		exp.eval(cxt);
 
-		return false;
+		// process and execute solutions
+		return details::execute (cxt, s_range);
 	}
 
 }
@@ -639,11 +436,11 @@ int main(int arg_c, char * arg_v[]) {
 	auto usage_default_values = +any()[&demo::files];
 	
 	auto expression =
-		usage_options |
-		usage_options >> usage_default_values;
+		usage_options >> usage_default_values |
+		usage_options
 	;
 	
-	parse(expression, 7, args);
+	parse (expression, 7, args);
 	
 	//auto val = command_line::value(&demo::xxx, .0F);
 
